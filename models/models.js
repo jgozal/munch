de4164dbb26eb975067f48b1b60e2b9502b495be
Jimmy Gozal <jgozal@developerkit.co>
2016-11-03 20:32:42 -0400

    added models and routing

--- models/models.js
+++ models/models.js
@@ -2,12 +2,20 @@ let mongoose = require('mongoose');
 
 // Create a Schema
 let Ingredients = mongoose.Schema({
-  name: String
+  name: String,
+  price: Number,
+  inStock: Boolean
+});
+
+let Orders = mongoose.Schema({
+  number: Number,
+  ingredients: [String]
 });
 
 
 let models =  {
-  Ingredients : mongoose.model("ingredients", Ingredients)
+  Ingredients : mongoose.model("ingredients", Ingredients),
+  Orders : mongoose.model("orders", Orders)
 }
 
 module.exports = models;
--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,14 @@ let router = express.Router();
 
 router.get('/', rh.home);
 router.get('/ingredients', rh.sayHi);
+router.post('/ingredients/add', rh.sayHi);
+router.post('/ingredients/disable', rh.sayHi);
+router.post('/ingredients/edit', rh.sayHi);
+router.get('/order/', rh.sayHi);
+router.post('/order/make', rh.sayHi);
+router.get('/kitchen', rh.sayHi);
+router.delete('/kitchen/complete', rh.sayHi);
+
 
 module.exports = router;
 
