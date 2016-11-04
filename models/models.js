5d218c6793660823e516d853a70f8730754f6c35
Jimmy Gozal <jgozal@developerkit.co>
2016-11-03 20:46:39 -0400

    added commenting on routing

--- models/models.js
+++ models/models.js
@@ -1,3 +1,5 @@
+'use strict'
+
 let mongoose = require('mongoose');
 
 // Create a Schema
--- routes/routes.js
+++ routes/routes.js
@@ -5,14 +5,17 @@ let rh = require('../routes/requestHandlers');
 let router = express.Router();
 
 router.get('/', rh.home);
-router.get('/ingredients', rh.sayHi);
-router.post('/ingredients/add', rh.sayHi);
-router.post('/ingredients/disable', rh.sayHi);
-router.post('/ingredients/edit', rh.sayHi);
-router.get('/order/', rh.sayHi);
-router.post('/order/make', rh.sayHi);
-router.get('/kitchen', rh.sayHi);
-router.delete('/kitchen/complete', rh.sayHi);
+
+router.get('/ingredients', rh.sayHi); // Shows list of ingredients (name & price) with Out-of-Stock, add, and edit buttons
+router.post('/ingredients/add', rh.sayHi); // Adds a new ingredient to the database and displays it
+router.post('/ingredients/disable', rh.sayHi); // Assigns False boolean to inStock property of Ingredient in database and shows ingredient as out-of-stock on page
+router.post('/ingredients/edit', rh.sayHi); // Updates name and price for the ingredient and reflects this change on the page
+
+router.get('/order/', rh.sayHi); // Shows a form with a checklist of ingredients to submit orders
+router.post('/order/make', rh.sayHi); // Creates and order
+
+router.get('/kitchen', rh.sayHi); // Shows all pending orders
+router.delete('/kitchen/complete', rh.sayHi); // Deletes order from pending orders
 
 
 module.exports = router;
